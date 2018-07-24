3d4baa96bdca228411385a8091f60cf57cb342e9
Cecep Aprilianto <cecep@qwords.com>
2018-07-25 02:02:15 +0700

    use medium library

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { AuthSession } from "expo";
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-import { Medium } from "./src";
+// import Medium  from "./src";
 
 export default class App extends React.Component {
 
   handlePress = async () => {
-    const result = await Medium.logInAsync({
-      'client_id': '70eaa8fd5369',
-      'scope': Medium.getScopes(),
-      'state': 'my arbitary state'
-    })
+
+    var m = require('./src')
+
+    let medium = new m.Medium({
+      client_id: '70eaa8fd5369',
+      client_secret: 'f3c21130d72eafc0046dd8506051fdbc2b9e3b1f'
+    });
+
+    var redirect_url = AuthSession.getRedirectUrl();
+
+    var authUrl = medium.getAuthorizationUrl('my state', redirect_url, medium.scopes());
+
+    const result = await AuthSession.startAsync({
+      authUrl: authUrl
+    });
 
     console.log(result);
   }
